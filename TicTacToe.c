dee9d31c9c90b9cb4965bc0c8f82e86a64f39088
Niall Swan <46677948+Mekstep@users.noreply.github.com>
2019-03-26 14:41:30 +0000

    3. Errors Fixed, now displays correctly

--- TicTacToe.c
+++ TicTacToe.c
@@ -5,77 +5,80 @@
 //Niall Swan 40207307
 
 //board squares
-const NOUGHTS = 1;
-const CROSSES = 2;
-const BORDER = 3;
-const EMPTY = 0;
+enum { NOUGHTS, CROSSES, BORDER, EMPTY };
+enum { HUMANWIN, COMPWIN, DRAW };
 
 //playable board locations
-const int ConvertTo25[9] = {	
-	6,7,8
-	11,12,13
-	16,17,18	
-}
+const int ConvertTo25[9] = {
+	
+	6,7,8,
+	11,12,13,
+	16,17,18
+	
+};
 
 //set up the board
-void InitialiseBoard(int *board)
-{
-	int index= 0;
+void InitialiseBoard(int *board) {
+	int index = 0;
 	
-	for(index = 0; index < 25; index++)
-	{
-		board[index] == BORDER;
+	for(index = 0; index < 25; ++index) {
+		
+		board[index] = BORDER;
 	}
 	
 	//get playable locations
-		for(index = 0; index < 9; index++)
-	{
-		board[ConvertTo25[index]] == EMPTY;
-	}
+	for(index = 0; index < 9; ++index) {
+		
+		board[ConvertTo25[index]] = EMPTY;
+	}	
 }
 
-void PrintBoard(const int *board)
-{
+void PrintBoard(const int *board) {
 	int index = 0;
-	
-	char pieces[] = "OX|-";
 	
 	printf("\nBoard:\n");
 	
 	//print squares
-	for(index= 0; index < 25; index++)
-	{
+	for(index = 0; index < 25; ++index) {
+		
 		//create a new line after every 5 squares
-		if(index!=0 && index%5==0)
-		{
+		if(index!=0 && index%5==0) {
+			
 			printf("\n");
 		}
 		
-		printf("%4c", pieces[board[ConvertTo25[index]]]);
+		printf("%4d",board[index]);
 	}
 	
 	printf("\n");
 }
 
 //run the game
-void RunGame()
-{
+void RunGame() {
+
+	int GameOver = 0;
+	int Side = NOUGHTS;
+	int LastMoveMade = 0;
+	
 	//declare board
 	int board[25];
-	
 	//initialise board
 	InitialiseBoard(&board[0]);
-		
 	//print board
 	PrintBoard(&board[0]);
+	
 }
 
-
-int main()
-{
-	RunGame();
+int main() {		
 	
+	srand(time(NULL));
+	RunGame();
+		
 	return 0;
 }
 
-*/
+
+
+
+
+
