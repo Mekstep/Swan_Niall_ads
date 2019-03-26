47d2ceb1403f88ad237771adac1e0e5e1d9ef8d8
Niall Swan <46677948+Mekstep@users.noreply.github.com>
2019-03-26 17:17:24 +0000

    5. Win conditions added, choice between pvp or pve added

--- TicTacToe.c
+++ TicTacToe.c
@@ -8,6 +8,9 @@
 enum { NOUGHTS, CROSSES, BORDER, EMPTY };
 enum { HUMANWIN, COMPWIN, DRAW };
 
+//directions
+const int directions[4] = {1, 5, 4, 6};
+
 //playable board locations
 const int ConvertTo25[9] = 
 {	
@@ -16,6 +19,58 @@ const int ConvertTo25[9] =
 	16,17,18
 	
 };
+
+//get number for direction
+int GetNumForDir(int startSq, const int dir, const int *board, const int us) 
+{
+	int found = 0;
+	
+	//while it has not hit the border / if it hits the border stop
+	while(board[startSq] != BORDER) 
+	{	//
+		if(board[startSq] != us) 
+		{	
+			break;
+		}
+		found++;	
+		startSq += dir;
+	}	
+	return found;
+}
+
+//find 3 in a row
+int FindThreeInARow(const int *board, const int ourindex, const int us) 
+{
+
+	int DirIndex = 0;
+	
+	//direction
+	int Dir = 0;
+	
+	//count of how many in a row
+	int threeCount = 1;
+	
+	//for each direction check if there is 3 in a row
+	for(DirIndex = 0; DirIndex < 4; ++DirIndex) 
+	{
+		Dir = directions[DirIndex];
+		
+		threeCount += GetNumForDir(ourindex + Dir, Dir, board, us);
+		threeCount += GetNumForDir(ourindex + Dir * -1, Dir * -1, board, us);
+		
+		//if three found break
+		if(threeCount == 3) 
+		{
+			break;
+		}
+		
+		//reset count to 1
+		threeCount = 1;
+	}
+	
+	return threeCount;
+}
+
 
 //set up the board
 void InitialiseBoard(int *board) 
@@ -41,7 +96,7 @@ void PrintBoard(const int *board)
 {
 	int index = 0;
 	char pieces[] = "OX|-";
-	printf("\nBoard:\n");
+	printf("\n\nBoard:\n\n");
 	
 	//print squares
 	for(index = 0; index < 9; ++index) 
@@ -51,7 +106,7 @@ void PrintBoard(const int *board)
 		if(index!=0 && index%3==0) 
 		{
 			
-			printf("\n");
+			printf("\n\n");
 		}
 		
 		printf("%4c",pieces[board[ConvertTo25[index]]]);
@@ -60,6 +115,7 @@ void PrintBoard(const int *board)
 	printf("\n");
 }
 
+//check to see if board location is empty
 int HasEmpty(const int *board) 
 {
 	int index = 0;
@@ -72,8 +128,10 @@ int HasEmpty(const int *board)
 }
 
 
+//make move
 void MakeMove(int *board, const int sq, const side) 
 {
+	//place noughts or cross on location
 	board[sq] = side;
 }
 
@@ -87,6 +145,7 @@ int GetPlayerMove(const int *board)
 	int moveOk = 0;
 	int move = -1;
 	
+	//check to see if legal move, if it is change moveOk and return move
 	while (moveOk == 0) 
 	{
 	
@@ -131,6 +190,8 @@ int GetPlayerMove(const int *board)
 			printf("Square not available\n");
 			continue;
 		}
+		
+		//if pass move is ok
 		moveOk = 1;
 	}
 	
@@ -181,43 +242,167 @@ void RunGame() {
 	//print board
 	PrintBoard(&board[0]);
 	
+		//while game is not over
 		while(!GameOver) 
 		{
+			//noughts turn
 			if(Side==NOUGHTS) 
 			{		
-				// get player 1 move
+				//get player 1 move
 				LastMoveMade = GetPlayerMove(&board[0]);
+				//make player 1 mover
 				MakeMove(&board[0], LastMoveMade, Side);
+				//change sides
 				Side = CROSSES;
+				//print board
 				PrintBoard(&board[0]);
 			} 
+			//crosses turn
 			else 
 			{
-				// get player 2 move
+				//get player 2 move
 				LastMoveMade = GetPlayerMove(&board[0]);
+				//make player 2 move
 				MakeMove(&board[0], LastMoveMade, Side);
+				//change sides
 				Side = NOUGHTS;
+				//print board
 				PrintBoard(&board[0]);
 			}
+			
+			//if three in a row is found game over
+			if(FindThreeInARow(board, LastMoveMade, Side^1) == 3)
+			{
+				//game over
+				printf("Game over!\n");
+				GameOver = 1;
+				
+				//if noughts wins 
+				if(Side==NOUGHTS) 
+				{
+					printf("CROSSES Wins!\n");
+				} 
+				//if crosses wins
+				else 
+				{
+					printf("NOUGHTS Wins!\n");
+				}
+			}
+			
+			//draw
+			if(!HasEmpty(board))
+			{
+				printf("Game over! \n");
+				GameOver = 1;
+				printf("It's a draw\n");
+			}
+		}	
+					
+}
 
-		}
-		
-		//draw
-		if(!HasEmpty(board))
-		{
-			printf("Game over! \n");
-			GameOver = 1;
-			printf("It's a draw\n");
-		}
-		
+//run the game
+void RunGameVsAI() {
+
+	int GameOver = 0;
+	int Side = NOUGHTS;
+	int LastMoveMade = 0;
 	
+	//declare board
+	int board[25];
+	//initialise board
+	InitialiseBoard(&board[0]);
+	//print board
+	PrintBoard(&board[0]);
+	
+		//while game is not over
+		while(!GameOver) 
+		{
+			//noughts turn
+			if(Side==NOUGHTS) 
+			{		
+				//get player 1 move
+				LastMoveMade = GetPlayerMove(&board[0]);
+				//make player 1 mover
+				MakeMove(&board[0], LastMoveMade, Side);
+				//change sides
+				Side = CROSSES;
+				//print board
+				PrintBoard(&board[0]);
+			} 
+			//crosses turn
+			else 
+			{
+				//get player 2 move
+				LastMoveMade = GetCompMove(&board[0]);
+				//make player 2 move
+				MakeMove(&board[0], LastMoveMade, Side);
+				//change sides
+				Side = NOUGHTS;
+				//print board
+				PrintBoard(&board[0]);
+			}
+			
+			//if three in a row is found game over
+			if(FindThreeInARow(board, LastMoveMade, Side^1) == 3)
+			{
+				//game over
+				printf("Game over!\n");
+				GameOver = 1;
+				
+				//if noughts wins 
+				if(Side==NOUGHTS) 
+				{
+					printf("CROSSES Wins!\n");
+				} 
+				//if crosses wins
+				else 
+				{
+					printf("NOUGHTS Wins!\n");
+				}
+			}
+			
+			//draw
+			if(!HasEmpty(board))
+			{
+				printf("Game over! \n");
+				GameOver = 1;
+				printf("It's a draw\n");
+			}
+		}	
+					
 }
 
 int main() {		
 	
 	srand(time(NULL));
-	RunGame();
+	
+	char selection;
+	int selectionMade = 0;
 		
+	//prompt
+	printf("Press 1 to play against a player\n");
+	printf("Press 2 to play against AI\n");	
+		
+	//check to see if legal move, if it is change moveOk and return move
+	while (selectionMade == 0) 
+	{	
+		scanf("%d", &selection); 
+				
+		selectionMade = 1;
+	}
+	
+	if(selectionMade = 1)
+	{
+		if(selection = 1)
+		{
+			RunGame();
+		}
+		else if(selection = 2)
+		{
+			RunGameVsAI();
+		}
+	}
+	
 	return 0;
 }
 
